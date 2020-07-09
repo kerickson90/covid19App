b3a2b3193cdf5abc34444706182c75ae51f7fd3c
Katherine Erickson <kerickson90@gmail.com>
2020-07-09 12:51:40 -0700

    Updates

---
+++ precache-manifest.723f99c6a36e0cf992536af9d18908dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6b12d79663b23e2baf78c36f69b793",
+    "url": "/covid19App/index.html"
+  },
+  {
+    "revision": "289b47dc75fdd6e7a316",
+    "url": "/covid19App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79aee0d4a238ed965bad",
+    "url": "/covid19App/static/js/2.796f93e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19App/static/js/2.796f93e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289b47dc75fdd6e7a316",
+    "url": "/covid19App/static/js/main.0eeb974e.chunk.js"
+  },
+  {
+    "revision": "f4dbe0111b57b54417e8",
+    "url": "/covid19App/static/js/runtime-main.713bcd41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19App/static/media/logo.5d5d9eef.svg"
+  }
+]);
